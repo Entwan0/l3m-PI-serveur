c7e676d57f5535c41824f3b8596f840bb01e04d4
antoine volozan <volozan.antoine@gmail.com>
2021-04-13 15:57:50 +0200

    ajout fonctionnalites

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -2,4 +2,21 @@ package com.example;
 
 public class User {
     public String description;
+
+    public int age;
+    public String login;
+    
+<<<<<<< HEAD
+    public User(){
+        
+    }
+=======
+>>>>>>> 5a8651d1e3ce6c6c9da32c108d6083f2da39b917
+
+    public int getAge(){
+        return this.age;
+    }
+    public String getLogin(){
+        return this.login;
+    }
 }
